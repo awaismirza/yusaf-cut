0c9b2045aed8df2353a8214b8098f78f238777e3
Awais Jamil <awais@myocchealthrecord.com>
2026-06-06 00:12:47 +1000

    fix: make toolbar dropdown menus opaque

--- tailwind.config.js
+++ tailwind.config.js
@@ -30,6 +30,14 @@ export default {
           DEFAULT: "hsl(var(--destructive))",
           foreground: "hsl(var(--destructive-foreground))",
         },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
       },
       borderRadius: {
         lg: "var(--radius)",
