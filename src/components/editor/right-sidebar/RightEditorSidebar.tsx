807ea0558472193a8034283dc9d0f0eb9f892c6f
Awais Jamil <awais@myocchealthrecord.com>
2026-06-28 14:56:59 +1000

    chore: hide tool rail; inspector always visible until multiple panels added

--- src/components/editor/right-sidebar/RightEditorSidebar.tsx
+++ src/components/editor/right-sidebar/RightEditorSidebar.tsx
@@ -1,10 +1,8 @@
-import { RightToolRail } from "./RightToolRail";
 import { RightInspectorPanel } from "./RightInspectorPanel";
 
 export function RightEditorSidebar() {
   return (
     <div className="right-editor-sidebar">
-      <RightToolRail />
       <RightInspectorPanel />
     </div>
   );
--- src/components/editor/right-sidebar/RightInspectorPanel.tsx
+++ src/components/editor/right-sidebar/RightInspectorPanel.tsx
@@ -1,25 +1,14 @@
-import { useEditorUiStore } from "@/stores/editorUiStore";
 import { CombinedPanel } from "./panels/CombinedPanel";
 
 export function RightInspectorPanel() {
-  const inspectorOpen = useEditorUiStore((s) => s.inspectorOpen);
-
   return (
-    <div
-      className="right-inspector-panel"
-      style={{ width: inspectorOpen ? 320 : 0 }}
-      aria-hidden={!inspectorOpen}
-    >
-      {inspectorOpen && (
-        <>
-          <div className="inspector-panel-header">
-            <span className="inspector-panel-title">Tools</span>
-          </div>
-          <div className="inspector-panel-scroll">
-            <CombinedPanel />
-          </div>
-        </>
-      )}
+    <div className="right-inspector-panel" style={{ width: 320 }}>
+      <div className="inspector-panel-header">
+        <span className="inspector-panel-title">Tools</span>
+      </div>
+      <div className="inspector-panel-scroll">
+        <CombinedPanel />
+      </div>
     </div>
   );
 }
