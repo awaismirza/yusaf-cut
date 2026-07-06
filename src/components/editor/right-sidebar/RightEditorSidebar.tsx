4ba5d831521bc47704db08e73dac4aac793c32fb
Awais Jamil <owaesmirza@gmail.com>
2026-07-06 14:01:16 +1000

    feat: move editor sidebar to the left with mode-aware tool rail

--- src/components/editor/right-sidebar/RightEditorSidebar.tsx
+++
@@ -1,9 +0,0 @@
-import { RightInspectorPanel } from "./RightInspectorPanel";
-
-export function RightEditorSidebar() {
-  return (
-    <div className="right-editor-sidebar">
-      <RightInspectorPanel />
-    </div>
-  );
-}
